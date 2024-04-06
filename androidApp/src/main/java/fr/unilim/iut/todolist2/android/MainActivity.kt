0ee39cd1fc071202f0a9ca027d6a4f3c56f761d2
LacroixEnzo <enzo.lacroix@etu.unilim.fr>
2024-04-06 23:19:48 +0200

    more visibility for UI

--- androidApp/src/main/java/fr/unilim/iut/todolist2/android/MainActivity.kt
+++ androidApp/src/main/java/fr/unilim/iut/todolist2/android/MainActivity.kt
@@ -4,13 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
@@ -77,11 +81,13 @@ fun test_repo(repository: Repository){
 }
 @Composable
 fun button_add_task() {
-    val context = LocalContext.current
-    Button(onClick = {
-        context.startActivity(Intent(context, NewTaskActivity::class.java))
-    }) {
-        Text("+")
+    Column (modifier = Modifier.fillMaxSize().padding(end = 10.dp), horizontalAlignment = Alignment.End, verticalArrangement = Arrangement.Bottom, ){
+        val context = LocalContext.current
+        Button(onClick = {
+            context.startActivity(Intent(context, NewTaskActivity::class.java))
+        }) {
+            Text("+")
+        }
     }
 }
 
